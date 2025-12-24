82f04d3fd559c20750194d49f04ede5192b71b24
Анастасия Стрельник <a.strelnik@consult-info.ru>
2025-12-24 22:34:37 +0200

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,32 +19,32 @@ def browser_management():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     #
-    options = Options()
-    # options.add_argument('--headless=new')
-    browser.config.driver_options = options
-
     # options = Options()
     # # options.add_argument('--headless=new')
-    # selenoid_capabilities = {
-    #     "browserName": "chrome",
-    #     "browserVersion": "128.0",
-    #     "selenoid:options": {
-    #         "enableVNC": True,
-    #         "enableVideo": True
-    #     }
-    # }
-    #
-    # selenoid_login = os.getenv("SELENOID_LOGIN")
-    # selenoid_pass = os.getenv("SELENOID_PASS")
-    # selenoid_url = os.getenv("SELENOID_URL")
-    #
-    # options.capabilities.update(selenoid_capabilities)
-    # driver = webdriver.Remote(
-    #     # command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
-    #     command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-    #     options=options)
-    #
-    # browser.config.driver = driver
+    # browser.config.driver_options = options
+
+    options = Options()
+    # options.add_argument('--headless=new')
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "128.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        # command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
     yield
 
     attach.add_screenshot(browser)
